1ec977fa9f04f169050e0dbc5e563db34e1c4a7d
Glauco <gllavolpini@gmail.com>
2026-05-15 16:56:39 -0300

    fix: aumentar limite de upload de video

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    middlewareClientMaxBodySize: "500mb",
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "d2xsxph8kpxj0f.cloudfront.net" },
